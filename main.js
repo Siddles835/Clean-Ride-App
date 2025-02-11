890f5a22bac1af3899d87ca62f4e4c819b9a27f4
Siddles835 <sidkapoor6469@outlook.com>
2025-02-10 21:11:58 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+let username = document.getElementById("fullName").innerHTML;
+
+function Transfer(){
+    window.location.href = "DriverSignIn.html";
+}
+
+function Transfer2(){
+    window.location.href = "CustomerSignIn.html";
+}
+
+function goBack(){
+    window.location.href = "CustomerSignIn.html";
+}
+
+function goBack2(){
+    window.location.href = "CustomerSignIn.html";
+}
+
+function goBack3(){
+    window.location.href = "CustomerSignIn.html";
+}
+
+function Route346(){
+    window.location.href = "Route346.html";
+}
+
+function Route347(){
+    window.location.href = "Route347.html";
+}
+
+function Route348(){
+    window.location.href = "Route348.html";
+}
+
+function Home(){
+    window.location.href = "index.html";
+}
+
+function BusBooking(){
+    window.location.href = "DriverAssign.html";
+}
+
+function AnewBack(){
+    window.location.href = "DriverSignIn.html";
+}
+
+function Sign(){
+    window.location.href = "DriverScreen.html";
+}
+
+function RideShareBooking(){
+    window.location.href = "Rideshare.html";
+}
